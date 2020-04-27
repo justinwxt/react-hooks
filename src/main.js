f62305c41d5cf520b02d300bdfbb9bc2bb677dad
justin.wang <justin.wang@slamtec.com>
2020-04-27 17:45:12 +0800

    basic study

--- src/main.js
+++ src/main.js
@@ -1,19 +1,14 @@
-import React,{Component} from 'react'
-import ReactDOM from 'react-dom'
-import Example from './pages/hookState/countExample'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+// import Example from './pages/hookState/countExample'
+// import Example from './pages/apis/useState'
+// import Example from './pages/apis/useContext'
+// import Example from './pages/apis/useReducer'
+import Example from "./pages/apis/useRef";
 
-
-
-
-
-
-
-
-
-
-
-
-
-ReactDOM.render(<div>
-    <Example/>
-</div>,document.getElementById('root'))
+ReactDOM.render(
+  <div>
+    <Example />
+  </div>,
+  document.getElementById("root")
+);
---
+++ src/pages/apis/useState.js
@@ -0,0 +1,15 @@
+import React,{useState} from 'react'
+function Counter({initialCount = 0} = {}) {
+    const [count, setCount] = useState(initialCount);
+    console.log('re render')
+    return (
+      <>
+        Count: {count}
+        <button onClick={() => setCount(initialCount)}>Reset</button>
+        <button onClick={() => setCount(count - 1)}>-</button>
+        <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+      </>
+    );
+  }
+
+  export default Counter
